bd64572be79a2c237fec1a300358f75e11d49c40
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2018-09-06 19:24:51 +0100

    Final Bits and Pizzas with FAB and SnackBar

--- BitsAndPizzas/app/src/main/java/com/antz/bitsandpizzas/OrderActivity.kt
+++ BitsAndPizzas/app/src/main/java/com/antz/bitsandpizzas/OrderActivity.kt
@@ -2,7 +2,10 @@ package com.antz.bitsandpizzas
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.widget.Toolbar
+import android.view.View
+import android.widget.Toast
 
 class OrderActivity : AppCompatActivity() {
 
@@ -13,5 +16,17 @@ class OrderActivity : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    fun onClickDone(view : View) {
+        val s = "Your order has been updated"
+        val duration = Snackbar.LENGTH_SHORT
+        val snackBar = Snackbar.make(findViewById(R.id.order_coordinator), s, duration)
+        snackBar.setAction("Undo", View.OnClickListener {
+            val makeText = Toast.makeText(this, "Undone !", Toast.LENGTH_SHORT)
+            makeText.show()
+        })
+
+        snackBar.show()
     }
 }
