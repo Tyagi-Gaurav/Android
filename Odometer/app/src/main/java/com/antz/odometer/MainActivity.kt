87dc73d10de3cd0fb046d0a2f8d43cea86636518
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2018-09-24 21:49:58 +0100

    Odometer - Activity to Service Binding

--- Odometer/app/src/main/java/com/antz/odometer/MainActivity.kt
+++ Odometer/app/src/main/java/com/antz/odometer/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.antz.odometer
 
 import android.app.Activity
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.IBinder
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.NotificationCompat
+import android.support.v4.content.ContextCompat
 import android.widget.TextView
 import java.util.*
 
@@ -31,8 +37,53 @@ class MainActivity : Activity() {
 
     override fun onStart() {
         super.onStart()
-        val intent = Intent(this, OdometerService::class.java)
-        bindService(intent, connection, Context.BIND_AUTO_CREATE)
+
+        if (ContextCompat.checkSelfPermission(this, OdometerService.PERMISSION_STRING) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(OdometerService.PERMISSION_STRING),
+                    PERMISSION_REQUEST_CODE
+            )
+        } else {
+            val intent = Intent(this, OdometerService::class.java)
+            bindService(intent, connection, Context.BIND_AUTO_CREATE)
+        }
+    }
+
+    private val NOTIFICATION_ID: Int = 423
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<out String>?,
+                                            grantResults: IntArray?) {
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+                    if (grantResults!!.isNotEmpty() &&
+                            grantResults[0] == PERMISSION_REQUEST_CODE) {
+                        val intent = Intent(this, OdometerService::class.java)
+                        bindService(intent, connection, Context.BIND_AUTO_CREATE)
+                    } else {
+                        val builder = NotificationCompat.Builder(this, "DEFAULT")
+                                .setSmallIcon(android.R.drawable.ic_menu_compass)
+                                .setContentTitle(resources.getString(R.string.app_name))
+                                .setContentText(resources.getString(R.string.permission_denied))
+                                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                                .setVibrate(longArrayOf(1000, 1000))
+                                .setAutoCancel(true)
+                        val actionIntent = Intent(this, MainActivity::class.java)
+                        val actionPendingIntent = PendingIntent.getActivity(this,
+                                0,
+                                actionIntent,
+                                PendingIntent.FLAG_UPDATE_CURRENT)
+                        builder.setContentIntent(actionPendingIntent)
+
+                        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
+                                as NotificationManager
+                        notificationManager.notify(NOTIFICATION_ID, builder.build())
+
+                    }
+
+            }
+        }
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,6 +119,10 @@ class MainActivity : Activity() {
 
             handler.postDelayed(this, 1000)
         }
+    }
+
+    companion object {
+        const val PERMISSION_REQUEST_CODE = 689
     }
 
 }
