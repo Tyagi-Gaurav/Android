bb4f44563bf3f650706705112af1de27ab1161cc
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2018-10-11 23:42:39 +0100

    Get data from Calendar and Log it in LogCat

--- MyCalendar/app/src/main/java/com/antz/mycalendar/MyCalendarAdapter.kt
+++ MyCalendar/app/src/main/java/com/antz/mycalendar/MyCalendarAdapter.kt
@@ -14,23 +14,17 @@ class MyCalendarAdapter(private val contentResolver: ContentResolver,
                         private val activity: AppCompatActivity) {
 
     private val EVENT_PROJECTION: Array<String> = arrayOf(
-            CalendarContract.Calendars._ID,                     // 0
-            CalendarContract.Calendars.ACCOUNT_NAME,            // 1
-            CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,   // 2
-            CalendarContract.Calendars.OWNER_ACCOUNT,            // 3
-            CalendarContract.Calendars.ACCOUNT_TYPE            // 4
-
+            CalendarContract.Events.CALENDAR_ID,
+            CalendarContract.Events._ID,
+            CalendarContract.Events.ACCOUNT_NAME,
+            CalendarContract.Events.ACCOUNT_TYPE,
+            CalendarContract.Events.TITLE
     )
 
     companion object {
         // The indices for the projection array above.
         private const val PERMISSION_REQUEST_CODE = 689
         private const val READ_CALENDAR_PERMISSION_STRING = android.Manifest.permission.READ_CALENDAR
-        private const val PROJECTION_ID_INDEX: Int = 0
-        private const val PROJECTION_ACCOUNT_NAME_INDEX: Int = 1
-        private const val PROJECTION_DISPLAY_NAME_INDEX: Int = 2
-        private const val PROJECTION_OWNER_ACCOUNT_INDEX: Int = 3
-        private const val PROJECTION_ACCOUNT_TYPE_INDEX: Int = 4
     }
 
     fun getEvents() {
@@ -41,25 +35,21 @@ class MyCalendarAdapter(private val contentResolver: ContentResolver,
                     PERMISSION_REQUEST_CODE
             )
         } else {
-            val uri: Uri = CalendarContract.Calendars.CONTENT_URI
-            val selection: String = "((${CalendarContract.Calendars.ACCOUNT_NAME} = ?) AND (" +
-                    "${CalendarContract.Calendars.ACCOUNT_TYPE} = ?) AND (" +
-                    "${CalendarContract.Calendars.OWNER_ACCOUNT} = ?))"
+            val uri: Uri = CalendarContract.Events.CONTENT_URI
+            val selection: String = "((${CalendarContract.Events.ACCOUNT_NAME} = ?) AND (" +
+                    "${CalendarContract.Events.ACCOUNT_TYPE} = ?) AND (" +
+                    "${CalendarContract.Events.OWNER_ACCOUNT} = ?))"
             val selectionArgs: Array<String> = arrayOf("chonku@gmail.com", "com.google", "chonku@gmail.com")
-//            val selection = "(${CalendarContract.Calendars.VISIBLE} = ?)"
-//            val selectionArgs: Array<String> = arrayOf("1")
             val cur: Cursor = contentResolver.query(uri, EVENT_PROJECTION, selection, selectionArgs, null)
 
             while (cur.moveToNext()) {
                 // Get the field values
-                val calID: Long = cur.getLong(PROJECTION_ID_INDEX)
-                val displayName: String = cur.getString(PROJECTION_DISPLAY_NAME_INDEX)
-                val accountName: String = cur.getString(PROJECTION_ACCOUNT_NAME_INDEX)
-                val ownerName: String = cur.getString(PROJECTION_OWNER_ACCOUNT_INDEX)
-                val accountType: String = cur.getString(PROJECTION_ACCOUNT_TYPE_INDEX)
+                val calID: Long = cur.getLong(0)
+                val eventId: String = cur.getString(1)
+                val title: String = cur.getString(4)
                 // Do something with the values...
 
-                Log.d("MyCalendar", "$calID, $displayName, $accountName, $ownerName, $accountType")
+                Log.d("MyCalendar", "$eventId, $title")
             }
 
             Log.d("MyCalendar", "Cursor ${cur.count}")
